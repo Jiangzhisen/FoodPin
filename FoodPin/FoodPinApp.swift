3d207548e5b1b171e98f1bc51b91af8dd7049a0d
Jiangzhisen <zzzz4444450@gmail.com>
2024-10-20 19:01:23 +0800

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by 姜智森 on 2024/10/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
